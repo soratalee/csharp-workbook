f27de879eaf1d83afc947da53b8bd0c42b9bc441
Sung-ung Lee <soratalee@gmail.com>
2018-10-23 11:50:50 -0500

    Added tests and try/catch

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -33,27 +33,74 @@ namespace TicTacToe
 
             Console.WriteLine("Player " + playerTurn);
             Console.WriteLine("Enter Row:");
-            int row = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Column:");
-            int column = int.Parse(Console.ReadLine());
-            PlaceMark(row, column);
-        }
+            int row = 0;
+            int column = 0;
+            //Try function in case of invalid input
+            try
+            {
+                row = Convert.ToInt32((Console.ReadLine()));
+                //Test for only 0, 1, or 2 inputs for Row
+                if (test1(row))
+                {
+                    Console.WriteLine("Enter Column:");
 
+                    column = Convert.ToInt32((Console.ReadLine()));
+                    
+                    //Test for only 0, 1, or 2 inputs for Column
+                    if (test2(column))
+                    {
+                        //if Both test1 and test2 run through, PlaceMark will be set
+                        PlaceMark(row, column);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid placement or Column! Please enter 0, 1, or 2 only.");
+                        if (playerTurn == "X")
+                        {
+                            playerTurn = "O";
+                        }
+                        else playerTurn = "X";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid placement for Row! Please enter 0, 1, or 2 only.");
+                    if (playerTurn == "X")
+                    {
+                        playerTurn = "O";
+                    }
+                    else playerTurn = "X";
+                }
+            }
+            //catch function to notify user of bad input
+            catch
+            {
+                //resets the playerTurn
+                if (playerTurn == "X")
+                {
+                    playerTurn = "O";
+                }
+                else playerTurn = "X";
+                Console.WriteLine("Invalid placement! Please enter 0, 1, or 2 only.");
+            }
+        }
         public static void PlaceMark(int row, int column)
         {
-            // your code goes here
+            //If PlayerTurn is an X and the location is valid, set the PlaceMark
             if (playerTurn == "X" && board[row][column] == " ")
             {
                 board[row][column] = "X";
-                
+
             }
-            else if (playerTurn == "O" && board[row][column] == " ") 
+            //If PlayerTurn is an O and the location is valid, set the PlaceMark
+            else if (playerTurn == "O" && board[row][column] == " ")
             {
                 board[row][column] = "O";
 
             }
-            else 
+            else
             {
+                //If PlaceMark is already taken, notify user and reset PlayerTurn
                 Console.WriteLine("Place is already taken! Place your marker somewhere else.");
                 if (playerTurn == "X")
                 {
@@ -65,7 +112,7 @@ namespace TicTacToe
 
         public static bool CheckForWin()
         {
-            // your code goes here
+            // If player wins any direction
             if (HorizontalWin() || VerticalWin() || DiagonalWin())
             {
                 Console.WriteLine("Player " + playerTurn + " Won!");
@@ -77,8 +124,7 @@ namespace TicTacToe
 
         public static bool CheckForTie()
         {
-            // your code goes here
-
+            //Check for tie. If all spaces are taken and there is no win condition met
             if (
             (
                 (board[0][0] != " " && board[0][1] != " " && board[0][2] != " ") &&
@@ -97,7 +143,7 @@ namespace TicTacToe
 
         public static bool HorizontalWin()
         {
-            // your code goes here
+            //Win Condition for Horizontal
             return
             (
             (board[0][0] == playerTurn && board[0][1] == playerTurn && board[0][2] == playerTurn) ||
@@ -108,8 +154,7 @@ namespace TicTacToe
 
         public static bool VerticalWin()
         {
-            // your code goes here
-
+            //Win Condition for Veritcal
             return
             (
             (board[0][0] == playerTurn && board[1][0] == playerTurn && board[2][0] == playerTurn) ||
@@ -120,8 +165,7 @@ namespace TicTacToe
 
         public static bool DiagonalWin()
         {
-            // your code goes here
-
+            //Win Condition for Diagonal
             return
             (
             (board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2] == playerTurn) ||
@@ -131,12 +175,28 @@ namespace TicTacToe
 
         public static void DrawBoard()
         {
+            //Board visual display
             Console.WriteLine("  0 1 2");
             Console.WriteLine("0 " + String.Join("|", board[0]));
             Console.WriteLine("  -----");
             Console.WriteLine("1 " + String.Join("|", board[1]));
             Console.WriteLine("  -----");
             Console.WriteLine("2 " + String.Join("|", board[2]));
+        }
+
+        public static bool test1(int row)
+        {
+            //Test for valid Row number
+            if (row == 0 || row == 1 || row == 2)
+                return true;
+            else return false;
+        }
+        public static bool test2(int column)
+        {
+            //Test for valid Column number
+            if (column == 0 || column == 1 || column == 2)
+                return true;
+            else return false;
         }
     }
 }
