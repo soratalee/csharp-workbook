f4120e3ea18f75354ee03794510e6f5c025fb60e
Sung-ung Lee <soratalee@gmail.com>
2019-01-28 10:13:59 -0600

    new_hello_world

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace HelloWorld
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string name = "";
+        int age = 0;
+        int year = DateTime.Now.Year;
+
+
+        Console.WriteLine("Please enter your age: ");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        if (age < 18)
         {
-            Console.WriteLine("Hello Sung-ung!");
+            Console.WriteLine("Sorry you are too young to play");
+        }
+        else
+        {
+            Console.WriteLine("You are old enough to play.");
+            Console.WriteLine("Please enter your username: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}! You are {1} years old. It is the year {2}.", name, age, year);
+
         }
     }
 }
