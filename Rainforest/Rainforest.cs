7bfec9a328e780bbb4e92091909d77baaeb92536
Sung-ung Lee <soratalee@gmail.com>
2019-02-08 14:42:00 -0600

    rainforest progress

--- Rainforest/Rainforest.cs
+++ Rainforest/Rainforest.cs
@@ -7,59 +7,81 @@ namespace Rainforest
     {
         public static void Main()
         {
-            //Instantiate a new company called "Rainforest"
-            Company Rainforest = new Company("Rainforest");
-            Rainforest.addWarehouse("Houstin");
-            Rainforest.addWarehouse("Austin");
-
             Warehouse Houstin = new Warehouse("Houstin");
             Houstin.addContainerID("Houstin-01");
             Houstin.addContainerID("Houstin-02");
-
+            Houstin.addContainerID("Houstin-03");
+            
             Warehouse Austin = new Warehouse("Austin");
             Austin.addContainerID("Austin-01");
             Austin.addContainerID("Austin-02");
 
             Container Houstin01 = new Container("Houstin-01");
-            Houstin01.addItem("bananas");
-            Houstin01.addItem("apples");
+            Houstin01.addItemToContainer("bananas");
+            Houstin01.addItemToContainer("apples");
 
             Container Houstin02 = new Container("Houstin-02");
-            Houstin01.addItem("bread");
-            Houstin01.addItem("milk");
+            Houstin02.addItemToContainer("bread");
+            Houstin02.addItemToContainer("milk");
 
             Container Austin01 = new Container("Austin-01");
-            Austin01.addItem("strawberry");
-            Austin01.addItem("pineapple");
+            Austin01.addItemToContainer("strawberry");
+            Austin01.addItemToContainer("pineapple");
 
             Container Austin02 = new Container("Austin-02");
-            Austin02.addItem("orange juice");
-            Austin02.addItem("salad");
+            Austin02.addItemToContainer("orange juice");
+            Austin02.addItemToContainer("salad");
 
-            //Console.WriteLine(Austin01.getManifest());
-            foreach (object o in Warehouse)
-            {
-
-            }
-            Console.Read();
+            Console.WriteLine(Houstin01.printContainer());
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(Houstin.printWarehouse());
         }
     }
-    public class Company
+    public class Item
     {
-        public List<Warehouse> warehouseList = new List<Warehouse>();
-        public Company(string warehouseName)
+        //int id = 0;
+        private string itemName = null; //{get; private set;}
+        //int size = 0
+        public Item(string itemName)
         {
-            warehouseList.Add(new Warehouse(warehouseName));
+            this.itemName = itemName;
         }
-        public void addWarehouse(string name)
+        public string printItem()
         {
-            warehouseList.Add(new Warehouse(name));
+            return itemName;
+        }
+    }
+    public class Container
+    {
+        //int id = 0;
+        string containerName = null;
+        string itemName = null;
+        // /int size = 0;
+        public List<Item> itemList = new List<Item>();
+        public Container(string containerName)
+        {
+            this.containerName = containerName;
+        }
+        public void addItemToContainer(string item)
+        {
+            itemList.Add(new Item(itemName = item));
+        }
+        public string printContainer()
+        {
+            //Console.WriteLine(containerName);
+            
+            string result = "";
+            foreach (var o in itemList)
+            {
+                result+=o.printItem()+"\n";
+            }
+            return result;
         }
     }
     public class Warehouse
     {
-        string location = "";
-        string containerID = "";
+        string location = null;
+        string containerName = null;
         //int size = 0;
         public List<Container> containerList = new List<Container>();
         public Warehouse(string location)
@@ -68,41 +90,18 @@ namespace Rainforest
         }
         public void addContainerID(string containerID)
         {
-            containerList.Add(new Container(containerID));
+            containerList.Add(new Container(containerName = containerID));
         }
-        public string getID()
+        public string printWarehouse()
         {
-            return this.containerID;
-        }
-    }
-    public class Container
-    {
-        //int id = 0;
-        string itemName = "";
-        // /int size = 0;
-        public List<Item> itemList = new List<Item>();
-        public Container(string itemName)
-        {
-            this.itemName = itemName;
-        }
-        public void addItem(string item)
-        {
-            itemList.Add(new Item(itemName));
-        }
-        public string getitemName()
-        {
-            return this.itemName;
-        }
-    }
-    public class Item
-    {
-        //int id = 0;
-        string itemName = "";
-        //int size = 0;
-        public List<string> itemID = new List<string>();
-        public Item(string itemName)
-        {
-            this.itemName = itemName;
+           //Console.WriteLine(containerName);
+           
+           string result = null;
+           foreach (var o in containerList)
+           {
+               result+=o.printContainer()+"\n";
+           }
+           return result;
         }
     }
 }
