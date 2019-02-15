891c233a6bac404bd2ef6704fd8cf993319d6a37
Sung-ung Lee <soratalee@gmail.com>
2019-02-15 15:34:01 -0600

    Added item indexing

--- Rainforest/Rainforest.cs
+++ Rainforest/Rainforest.cs
@@ -11,8 +11,8 @@ namespace Rainforest
             Inventory inventorylist = new Inventory();
 
             //Instantiate new Warehouses
-            Warehouse Houstin = new Warehouse("Houstin", new Dictionary<string,Container>());
-            Warehouse Austin = new Warehouse("Austin", new Dictionary<string,Container>());
+            Warehouse Houstin = new Warehouse("Houstin", new Dictionary<string, Container>());
+            Warehouse Austin = new Warehouse("Austin", new Dictionary<string, Container>());
 
             //Instantiate new Containers
             Container Houstin01 = new Container("Houstin-01", new List<Item>());
@@ -31,14 +31,14 @@ namespace Rainforest
             Item pineapple = new Item("pineapple");
 
             //Insert Warehouse to Inventory
-            inventorylist.inventory.Add("Houstin",Houstin);
-            inventorylist.inventory.Add("Austin",Austin);
+            inventorylist.inventory.Add("Houstin", Houstin);
+            inventorylist.inventory.Add("Austin", Austin);
 
             //Insert Container to Warehouse
-            Houstin.containerList.Add("Houstin01",Houstin01);
-            Houstin.containerList.Add("Houstin02",Houstin02);
-            Austin.containerList.Add("Austin01",Austin01);
-            Austin.containerList.Add("Austin02",Austin02);
+            Houstin.containerList.Add("Houstin01", Houstin01);
+            Houstin.containerList.Add("Houstin02", Houstin02);
+            Austin.containerList.Add("Austin01", Austin01);
+            Austin.containerList.Add("Austin02", Austin02);
 
             //Insert Item to Container
             Austin01.itemList.Add(milk);
@@ -50,8 +50,14 @@ namespace Rainforest
             Houstin02.itemList.Add(juice);
             Houstin02.itemList.Add(ham);
             Houstin02.itemList.Add(pineapple);
-            
+
             inventorylist.printInventory();
+
+            //Index of Items
+            Console.WriteLine("Enter an item you want to look up from the list above: ");
+            string itemSearch = Console.ReadLine();
+
+            inventorylist.itemIndex(itemSearch);
         }
 
         public class Inventory
@@ -71,6 +77,24 @@ namespace Rainforest
                         }
                     }
                 }
+            }
+
+            public void itemIndex(string itemSearch)
+            {
+                foreach (KeyValuePair<string, Warehouse> warehouse in this.inventory)
+                {
+                    foreach (KeyValuePair<string, Container> containers in warehouse.Value.containerList)
+                    {
+                        foreach (Item item in containers.Value.itemList)
+                        {
+                            if (item.itemName == itemSearch)
+                            {
+                                Console.WriteLine("||" + itemSearch + " is available in {0}", containers.Key);
+                            }
+                        }
+                    }
+                }
+
             }
         }
         public class Item
@@ -98,9 +122,9 @@ namespace Rainforest
         {
             public string warehouseName { get; private set; }
             public Dictionary<string, Container> containerList = new Dictionary<string, Container>();
-                //public List<Container> containerList = new List<Container>();
+            //public List<Container> containerList = new List<Container>();
             //Creating a constructor for the class Container
-            public Warehouse(string warehouseName, Dictionary<string,Container> Containers)
+            public Warehouse(string warehouseName, Dictionary<string, Container> Containers)
             {
                 this.warehouseName = warehouseName;
                 this.containerList = Containers;
