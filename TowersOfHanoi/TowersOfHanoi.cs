ec9c4e027f77858679901e879b33bb28a29c4d95
Sung-ung Lee <soratalee@gmail.com>
2019-02-18 18:36:38 -0600

    additions

--- TowersOfHanoi/TowersOfHanoi.cs
+++ TowersOfHanoi/TowersOfHanoi.cs
@@ -20,30 +20,37 @@ namespace TowersOfHanoi
             Tower B = new Tower("B", new Stack<Block>());
             Tower C = new Tower("C", new Stack<Block>());
             
-            //Insert towers into dictionary
+            //Initial Insert towers into dictionary
             newGame.towers.Add("A", A);
             newGame.towers.Add("B", B);
             newGame.towers.Add("C", C);
 
             //Insert blocks to Towers
-            A.blocks.Push(one);
-            A.blocks.Push(two);
-            A.blocks.Push(three);
             A.blocks.Push(four);
+            A.blocks.Push(three);
+            A.blocks.Push(two);
+            A.blocks.Push(one);
+            
+            
+            //Move pieces
+            string consoleMoveFrom;
+            string consoleMoveTo;
 
+            Console.WriteLine("Enter in tower(A, B, or C) to move FROM:");
+            consoleMoveFrom = (Console.ReadLine());
+            
+            Console.WriteLine("Enter in tower(A, B, or C) to move TO:");
+            consoleMoveTo = (Console.ReadLine());
+
+            newGame.movePiece(consoleMoveFrom, consoleMoveTo);
             //Draw Tower
             newGame.printTowerKeys();
-
         }
     }
     public class Game
     {
         public Dictionary<string, Tower> towers = new Dictionary<string, Tower>();
-        
-        public void moveBlock()
-        {
 
-        }
         public void printTowerKeys()
         {
             foreach (KeyValuePair<string, Tower> tower in this.towers)
@@ -55,15 +62,38 @@ namespace TowersOfHanoi
                 }
             }
         }
+
+        public void movePiece(Tower moveFrom, Tower moveTo)
+        {
+            moveTo.blocks.Push(moveFrom.blocks.Peek());
+            moveFrom.blocks.Pop();
+        }
     }
+    
     public class Block
     {
         //Constructor for Block
         public Block(int weight)
         {
-            this.weight = weight;
+            //Visual representation of weights
+            if (weight == 1)
+            {
+               this.weight =  "1.    O";
+            };
+            if (weight == 2)
+            {
+               this.weight =  "2.   OOO";
+            };
+            if (weight == 3)
+            {
+               this.weight =  "3.  OOOOO";
+            };
+            if (weight == 4)
+            {
+               this.weight =  "4. OOOOOOO";
+            };
         }
-        public int weight { get; private set; }
+        public string weight { get; private set; }
     }
 
     public class Tower
